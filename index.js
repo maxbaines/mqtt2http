b5bc396de9c0019b687213c7ce751e49dcb053d1
Max <max@cncdata.co.uk>
2024-03-17 12:42:06 +0000

    return the html response on an mqtt topic

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config({
-  silent: true
+require("dotenv").config({
+  silent: true,
 });
 
-const mqtt = require('mqtt');
-const request = require('request');
-const Handlebars = require('handlebars');
+const mqtt = require("mqtt");
+const request = require("request");
+const Handlebars = require("handlebars");
 
-Handlebars.registerHelper('stringify', object => {
+Handlebars.registerHelper("stringify", (object) => {
   return new Handlebars.SafeString(JSON.stringify(object));
 });
 
@@ -20,13 +20,23 @@ const DEBUG_NOOP = !!process.env.DEBUG_NOOP;
 const MQTT_URL = process.env.MQTT_URL;
 const MQTT_USERNAME = process.env.MQTT_USERNAME;
 const MQTT_PASSWORD = process.env.MQTT_PASSWORD;
-const MQTT_CONNECT = process.env.MQTT_CONNECT ? JSON.parse(process.env.MQTT_CONNECT) : {};
-const MQTT_TOPIC = IS_JSON.test(process.env.MQTT_TOPIC) ? JSON.parse(process.env.MQTT_TOPIC) : process.env.MQTT_TOPIC;
-const MQTT_SUBSCRIBE = process.env.MQTT_SUBSCRIBE ? JSON.parse(process.env.MQTT_SUBSCRIBE) : undefined;
+const MQTT_CONNECT = process.env.MQTT_CONNECT
+  ? JSON.parse(process.env.MQTT_CONNECT)
+  : {};
+const MQTT_TOPIC = IS_JSON.test(process.env.MQTT_TOPIC)
+  ? JSON.parse(process.env.MQTT_TOPIC)
+  : process.env.MQTT_TOPIC;
+const MQTT_SUBSCRIBE = process.env.MQTT_SUBSCRIBE
+  ? JSON.parse(process.env.MQTT_SUBSCRIBE)
+  : undefined;
 
-const HTTP_URL = process.env.HTTP_URL ? Handlebars.compile(process.env.HTTP_URL) : undefined;
+const HTTP_URL = process.env.HTTP_URL
+  ? Handlebars.compile(process.env.HTTP_URL)
+  : undefined;
 const HTTP_JSON = !!process.env.HTTP_JSON;
-const HTTP_REQUEST = process.env.HTTP_REQUEST ? Handlebars.compile(process.env.HTTP_REQUEST) : undefined;
+const HTTP_REQUEST = process.env.HTTP_REQUEST
+  ? Handlebars.compile(process.env.HTTP_REQUEST)
+  : undefined;
 
 if (MQTT_USERNAME) {
   MQTT_CONNECT.username = MQTT_USERNAME;
@@ -36,56 +46,75 @@ if (MQTT_PASSWORD) {
   MQTT_CONNECT.password = MQTT_PASSWORD;
 }
 
-let client = MQTT_URL ? mqtt.connect(MQTT_URL, MQTT_CONNECT) : mqtt.connect(MQTT_CONNECT);
+let client = MQTT_URL
+  ? mqtt.connect(MQTT_URL, MQTT_CONNECT)
+  : mqtt.connect(MQTT_CONNECT);
 
-client.on('connect', connack => {
-  debug('connect', connack);
+client.on("connect", (connack) => {
+  debug("connect", connack);
 
   client.subscribe(MQTT_TOPIC, MQTT_SUBSCRIBE, (err, granted) => {
-    debug('subscribe', err, granted);
+    debug("subscribe", err, granted);
   });
 });
 
-client.on('error', debug.bind(null, 'error'));
+client.on("error", debug.bind(null, "error"));
 
-client.on('message', (topic, message, packet) => {
-  debug('message', topic, message, packet);
+client.on("message", (topic, message, packet) => {
+  debug("message", topic, message, packet);
 
   let messageString = message.toString();
-  let messageObject = IS_JSON.test(messageString) ? JSON.parse(messageString) : undefined;
+  let messageObject = IS_JSON.test(messageString)
+    ? JSON.parse(messageString)
+    : undefined;
 
-  let options = HTTP_REQUEST ? JSON.parse(HTTP_REQUEST({
-    topic: topic,
-    message: messageObject || messageString
-  })) : {};
+  let options = HTTP_REQUEST
+    ? JSON.parse(
+        HTTP_REQUEST({
+          topic: topic,
+          message: messageObject || messageString,
+        })
+      )
+    : {};
 
   if (messageObject && HTTP_JSON) {
     options.json = true;
     options.body = {
       topic: topic,
-      message: messageObject
+      message: messageObject,
     };
   }
 
   if (HTTP_URL) {
     options.uri = HTTP_URL({
       topic: topic,
-      message: messageObject || messageString
+      message: messageObject || messageString,
     });
   }
 
-  debug('request', options);
+  debug("request", options);
 
   if (!DEBUG_NOOP) {
-    request(options, (error, response, body) => debug.bind(null, 'response'));
+    request(options, (error, response, body) => {
+      debug.bind(null, "response");
+
+      client.publish(
+        "te/response/" + options.body.topic,
+        JSON.stringify(options.body.message)
+      );
+    });
   }
 });
 
 if (DEBUG_PUBLISH > 0) {
   setInterval(() => {
-    client.publish(MQTT_TOPIC, DEBUG_MESSAGE || JSON.stringify({
-      time: (new Date()).toString()
-    }));
+    client.publish(
+      MQTT_TOPIC,
+      DEBUG_MESSAGE ||
+        JSON.stringify({
+          time: new Date().toString(),
+        })
+    );
   }, DEBUG_PUBLISH);
 }
 
@@ -93,6 +122,6 @@ function debug(...args) {
   if (!DEBUG_LOG) {
     return;
   }
-  args.unshift('[DEBUG]');
+  args.unshift("[DEBUG]");
   console.log.apply(null, args);
 }
